f5ef9b4d567eba4d20532ac20582b6707237c3ba
hrimwk <>
2022-08-10 09:57:46 +0900

    [ADD] 유저 회원가입 엔드포인트 및 포스팅 구현

---
+++ app.js
@@ -0,0 +1,58 @@
+const users = [
+  {
+    id: 1,
+    name: "User1",
+    email: "test1@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "User2",
+    email: "test2@gmail.com",
+    password: "123qwe",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },
+];
+
+const createUser = (req, res) => {
+  const user = req.body.data;
+
+  users.push({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    password: user.password,
+  });
+  console.log("after:", users);
+  res.json({ message: "USER_CREATE" });
+};
+
+const createPost = (req, res) => {
+  const post = req.body.data;
+
+  posts.push({
+    id: post.id,
+    title: post.title,
+    content: post.content,
+    userId: post.userId,
+  });
+
+  console.log("after:", posts);
+  res.json({ message: "CONTENT_CREATE" });
+};
+
+module.exports = { createUser, createPost };
---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const express = require("express");
+const { createUser, createPost } = require("./app");
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", createUser);
+app.post("/post", createPost);
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log("server is running on PORT 8000");
+});
